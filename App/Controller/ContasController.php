f9b4987b8faa07ec76054b66d57ab1b6ba5e897e
FelipeRodrigodosSantos <feliperodrigodossantosrodrigo@gmail.com>
2024-04-19 12:57:51 -0300

    criação de cadastro de contas

---
+++ App/Controller/ContasController.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'C:/xampp/htdocs/system_concessionaria/App/Model/ContasModel.php';
+
+
+class contasController {
+    private $contaModel;
+
+    public function __construct($pdo) {
+        $this->contaModel = new ContaModel($pdo);
+    }
+
+    public function criarConta($fornecedores, $salarios_benef, $aluguel, $contas_publicas, $impostos, $emprestimos, $manutencao, $seguros, $marketing, $despesas_adm, $logistica, $pesquisa, $garantia) {
+        $this->contaModel->criarConta($fornecedores, $salarios_benef, $aluguel, $contas_publicas, $impostos, $emprestimos, $manutencao, $seguros, $marketing, $despesas_adm, $logistica, $pesquisa, $garantia);
+    }
+
+    public function listarContas() {
+        return $this->contaModel->listarContas();
+    }
+
+    public function exibirListaContaes() {
+        $contaes = $this->contaModel->listarContas();
+        include 'C:/xampp/htdocs/system_concessionaria/Resources/Views/Contas/lista.php';
+    }
+
+    public function atualizarConta ($id_conta, $fornecedores, $salarios_benef, $aluguel, $contas_publicas, $impostos, $emprestimos, $manutencao, $seguros, $marketing, $despesas_adm, $logistica, $pesquisa, $garantia) {
+        $this->contaModel->atualizarConta($id_conta, $fornecedores, $salarios_benef, $aluguel, $contas_publicas, $impostos, $emprestimos, $manutencao, $seguros, $marketing, $despesas_adm, $logistica, $pesquisa, $garantia);
+    }
+    public function excluirConta ($id_conta) {
+        $this->contaModel->excluirConta($id_conta);
+    }
+}
+
+    
+?>

4551c74e3c031676dc8a33c3f4429c529090b1c5
FAB10-H3NRIQUE <fabiohpereira2007@hotmail.com>
2024-04-19 10:12:02 -0300

    MVC ESTOQUE

---
+++ App/Controller/EstoqueController.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'Model/EstoqueModel.php';
+
+class EstoqueController {
+    private $estoqueModel;
+
+    public function __construct($pdo) {
+        $this->estoqueModel = new EstoqueModel($pdo);
+    }
+       
+    public function criarEstoque($numero_referencia, $categoria, $quantidade, $preco_unitario, $fornecedor, $localizacao, $reabastecimento_minimo, $validade, $observacoes, $imagem) {
+       $this->estoqueModel->criarEstoque($numero_referencia, $categoria, $quantidade, $preco_unitario, $fornecedor, $localizacao, $reabastecimento_minimo, $validade, $observacoes, $imagem);
+     }
+
+    public function listarEstoque() {
+        return $this->estoqueModel->listarEstoque();
+    }
+
+    public function exibirListaEstoque() {
+        $estoques = $this->estoqueModel->listarEstoque();
+        include 'C:/xampp/htdocs/system_concessionaria/Resources/Views/Estoque/lista.php';
+    }
+
+    public function atualizarEstoque($id_estoque, $numero_referencia, $categoria, $quantidade, $preco_unitario, $fornecedor, $localizacao, $reabastecimento_minimo, $validade, $observacoes, $imagem) {
+        $this->estoqueModel->atualizarEstoque($id_estoque, $numero_referencia, $categoria, $quantidade, $preco_unitario, $fornecedor, $localizacao, $reabastecimento_minimo, $validade, $observacoes, $imagem);
+    }
+
+    public function excluirEstoque($id_estoque) {
+        $this->estoqueModel->excluirEstoque($id_estoque);
+    }
+}
+
+    
+?>

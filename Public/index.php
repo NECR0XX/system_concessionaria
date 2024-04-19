b38e26f9895ed21094e44e2ba276d0dfce7d3443
registramos <regis11052006@gmail.com>
2024-04-19 07:57:48 -0300

    regis- página inicial

---
+++ Public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Resources/Css/pagcss.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Dots&display=swap" rel="stylesheet">
+    <title>página inicial</title>
+</head>
+<body>
+<div class="container-header">
+    <p class="logo">SCAR</p>
+    <a href="" class="link-cadastro">Cadastrar</a>
+</div>
+        <img src="../Resources/Assets/S C A R.png">
+        <br><br><br><br><br>
+
+<div class="container-subinfo"> <p class="subinfo">Ao lado, disponibilizamos um tutorial de como utilizar nosso sistema. Acesse o documento e/ou assista o vídeo a seguir</p>
+
+<a href=""><img class="img" src="../Resources/Assets/documento-de-texto.png"></a>
+<a href=""><img class="vid" src="../Resources/Assets/video-player.png"></a>
+
+</div>
+</body>
+</html>

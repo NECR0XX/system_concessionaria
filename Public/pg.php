59408ebea67bdc50bcd92d4db53a29ae6455d4a7
registramos <regis11052006@gmail.com>
2024-04-24 08:17:24 -0300

    pagiana de configs

---
+++ Public/pg.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Site</title>
+    <link rel="stylesheet" href="../Resources/Css/stylepg.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Dots&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+    <aside>
+       <nav>
+        <p class="logo">SCAR
+    </p>
+        
+    <div class="search-container">
+    <form action="" method="get">
+        <input type="text" class="search-box" name="q" placeholder="">
+        <img src="../Resources/Assets/lupa.svg">
+    </form>
+</div>
+    <div class="ambiente">
+        <p>AMBIENTES</p>
+    </div>
+        <div>
+        <ul>
+            <li><button  class="links"><a href="#">Controle de pessoas</a></button></li>
+            <li><button  class="links"><a href="#">Controle fiscal</a></button></li>
+            <li><button  class="links"><a href="#">Controle de estoque</a></button></li>
+            <li><button  class="links"><a href="#">Controle de frota</a></button></li>
+            <li><button  class="links"><a href="#">Despesas</a></button></li>
+            <li><button  class="links"><a href="#">Comercial</a></button></li>
+        </ul></div>
+    </nav>
+    </aside>
+    <main>
+        <div class="container-main">
+            <p class="titulo">SELECIONE UM AMBIENTE</p>
+            <p class="subtitulo">e configure como quiser!</p>
+        </div>
+</main>
+</body>
+</html>

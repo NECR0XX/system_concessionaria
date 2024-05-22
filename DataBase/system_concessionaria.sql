9dc50b2e4194d6cebbc134d413ec26d1263d5ff2
veniceos <vinicius.canevari1@gmail.br>
2024-05-22 07:31:20 -0300

    to tentando envia o banco

--- DataBase/system_concessionaria.sql
+++ DataBase/system_concessionaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22-Maio-2024 às 12:23
+-- Tempo de geração: 22-Maio-2024 às 12:30
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 

4b3ebf38403321a0892ebab800400a5b6c4ee5dc
registramos <regis11052006@gmail.com>
2024-05-29 13:37:58 -0300

    tutorial do video e up do documento

---
+++ tutorial.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video Tutorial</title>
+    <link rel="stylesheet" href="Resources/Css/stylevid.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Video Tutorial</h1>
+        <video controls>
+            <source src="yt5s.io-TVアニメ「ヴィンランド・サガ」SEASON 2 第2クール ノンクレジットオープニングムービー_ 2nd Cour Textless Opening Video-(1080p).mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+</body>
+</html>

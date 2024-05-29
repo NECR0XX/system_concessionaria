0f89ae91493c1266b1b894cda5556b1d121a5b04
Ana Beatriz <anabeatrizcorreiaribeiro010@gmail.com>
2024-05-29 08:07:34 -0300

    barra

--- Public/Comercial/index.php
+++ Public/Comercial/index.php
@@ -31,12 +31,7 @@ if (isset($_POST['excluir_id_comercial'])) {
         <p class="logo">SCAR
     </p>
         
-    <div class="search-container">
-    <form action="" method="get">
-        <input type="text" class="search-box" name="q" placeholder="">
-        <img src="../../Resources/Assets/lupa.svg">
-    </form>
-</div>
+ 
     <div class="ambiente">
         <p>AMBIENTES</p>
     </div>
